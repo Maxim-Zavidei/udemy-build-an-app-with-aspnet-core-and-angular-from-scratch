8bba1d70025186736eea5b43ea88322ac1176601
Maxim Zavidei <maxim-zavidei+git@pm.me>
2023-04-16 09:58:44 +0200

    Added the rest web api files

--- src/App.Dating.WebApi/Program.cs
+++ src/App.Dating.WebApi/Program.cs
@@ -1,5 +1,12 @@
+using App.Dating.WebApi.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<DataDbContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 builder.Services.AddControllers();
 
 var app = builder.Build();
