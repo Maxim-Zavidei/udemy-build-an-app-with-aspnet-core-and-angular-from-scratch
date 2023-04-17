df5152dc72a797db981a02c3a81b943d566532e6
Maxim Zavidei <maxim-zavidei+git@pm.me>
2023-04-17 20:17:43 +0200

    Added Cors policy

--- src/App.Dating.WebApi/Program.cs
+++ src/App.Dating.WebApi/Program.cs
@@ -9,7 +9,12 @@ builder.Services.AddDbContext<DataDbContext>(opt =>
 });
 builder.Services.AddControllers();
 
+builder.Services.AddCors();
+
 var app = builder.Build();
+app.UseCors(builder =>
+    builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200")
+);
 
 app.MapControllers();
 
